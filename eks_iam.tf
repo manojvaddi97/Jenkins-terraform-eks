a4ef57a95486e1c1b29b42e4f064569b1b47dcb3
test-user-cc <manojvaddi497@gmail.com>
2024-07-27 21:15:53 -0400

    update eks iam policies

--- eks_iam.tf
+++ eks_iam.tf
@@ -8,12 +8,11 @@ resource "aws_iam_role" "eks_user_role" {
     Version = "2012-10-17"
     Statement = [
       {
-        Action = "sts:AssumeRole"
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
         Effect = "Allow"
-        Sid    = ""
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/manojvaddi"
+        }
+        Action = "sts:AssumeRole"
       }
     ]
   })
