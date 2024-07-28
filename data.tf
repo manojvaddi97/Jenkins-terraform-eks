3978e3b58029f8035ec909f1a082c2f294cc7c59
test-user-cc <manojvaddi497@gmail.com>
2024-07-28 02:18:24 -0400

    updated files

--- data.tf
+++ data.tf
@@ -1,21 +1,2 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"]
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-20240701.1"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
 
 data "aws_availability_zones" "azs" {}
